7a088338fd0b3042af8b81f59fb29f2809b71297
dev-rroslan <dev.rroslan@gmail.com>
2022-02-24 18:30:33 +0800

    edit Hero

--- components/Hero.js
+++ components/Hero.js
@@ -5,7 +5,7 @@ export default function Hero() {
     <div className="my-10 mx-auto max-w-7xl px-4 sm:mt-24 md:mt-10 text-center">
       <h1 className="font-extrabold text-gray-900">
         <p className="text-xl sm:text-3xl md:text-4xl">Boyboy Restaurant:</p>
-        <p className="text-transparent bg-clip-text bg-gradient-to-r from-purple-400 via-pink-500 to-red-500 text-4xl sm:text-6xl md:text-7xl">Modern eCommerce</p>
+        <p className="text-transparent bg-clip-text bg-gradient-to-r from-purple-400 via-pink-500 to-red-500 text-4xl sm:text-6xl md:text-7xl">E-Perniagaan</p>
       </h1>
       <h2 className="mt-3 max-w-md mx-auto text-gray-500 sm:text-lg md:mt-5 md:text-xl md:max-x-3xl">
         Restaurant Inovatif
